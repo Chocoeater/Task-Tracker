c5172813b28257a843ae128d68021873f593869d
Chocoeater <lonelypain98@gmail.com>
2025-09-11 21:40:33 +0300

    update docker-compose.dev.yml and settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6=or@5@+m-8qvx_pfta4x(gev$&1hp7yz!4ea(^e^9(wdnnepz'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
